48629d47b92576c4bc8f745b07e25f5643217f8c
Dmitry Olkhovikov <dimasic9420@gmail.com>
2021-01-30 17:57:15 +0200

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,47 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var coordsSchema = new Schema({
+    latitude: {
+        type: String,
+        default: ''
+    },
+    longitude: {
+        type: String,
+        default: ''
+    }
+});
+
+var User = new Schema({
+    fullname: {
+        type: String,
+        default: ''
+    },
+    telnum: {
+        type: String,
+        default: ''
+    },
+    coords: [coordsSchema],
+    timestamp: {
+        type: Date,
+        required: false
+    },
+    visible: {
+        type: Boolean,
+        default: true
+    },
+    facebookId: String,
+    admin: {
+        type: Boolean,
+        default: false
+    }
+},
+{
+    timestamps: true
+}
+);
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', User);
---
+++ routes/users.js
@@ -0,0 +1,169 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+var User = require('../models/user');
+var passport = require('passport');
+var authenticate = require('../authenticate');
+const cors = require('./cors');
+
+var router = express.Router();
+router.use(bodyParser.json());
+
+/* GET users listing. */
+router.options('*', cors.corsWithOptions, (req, res) => {res.sendStatus(200);});
+
+router.route('/')
+.get(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+  User.find({})
+    .then((users) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(users);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+router.post('/signup', cors.corsWithOptions, (req, res, next) => {
+  User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({err: err});
+    }
+    else {
+      if (req.body.fullname)
+        user.fullname = req.body.fullname;
+      if (req.body.telnum)
+        user.telnum = req.body.telnum;
+      if (req.body.coords)
+        user.coords = req.body.coords;
+      if (req.body.timestamp)
+        user.timestamp = req.body.timestamp;
+      if (req.body.visible)
+        user.visible = req.body.visible;  
+       
+        user.save((err, user) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({err: err});
+            return ;
+          }
+          passport.authenticate('local')(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({success: true, status: 'Registration succesful!'});
+        });
+      });
+    }
+  });
+});
+
+router.post('/login', cors.corsWithOptions, (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err)
+      return next(err);
+
+    if (!user) {
+      res.statusCode = 401;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success: false, status: 'Login unsuccessful!', err: info});
+    }
+
+    req.logIn(user, (err) => {
+      if (err) {
+        res.statusCode = 401;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: false, status: 'Login unsuccessful!', err: 'Could not log in the user!'});
+      }
+    })
+
+    var token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true, status: 'Login successful!', token: token});
+  }) (req, res, next);
+});
+
+router.get('/logout', (req, res, next) => {
+  if (req.session) {
+    req.session.destroy();
+    res.clearCookie('session-id');
+    res.redirect('/');
+  }
+  else {
+    var err = new Error('You are not logged in!');
+    err.statusCode = 403;
+    next(err);
+  }
+});
+
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+  if (req.user) {
+    var token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true, token: token, status: 'You are successfully logged in!'});
+  }
+});
+
+router.get('/checkJWTToken', cors.corsWithOptions, (req, res, next) => {
+  passport.authenticate('jwt', {session: false}, (err, user, info) => {
+    if (err)
+      return next(err);
+
+    if (!user) {
+      res.statusCode = 401;
+      res.setHeader('Content-tYpe', 'application/json');
+      return res.json({status: 'JWT invalid!', success: false, err: info});
+    }
+    else {
+      res.statusCode = 200;
+      res.setHeader('Content-tYpe', 'application/json');
+      return res.json({status: 'JWT valid!', success: true, user: user});
+    }
+  }) (req, res);
+});
+
+router.get('/account', cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+  User.findOne({_id: req.user._id})
+  .then((user) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(user);
+  }, err => next(err))
+  .catch(err => next(err));
+});
+
+router.put('/account', cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+  User.findByIdAndUpdate(req.user._id, {$set: req.body}, {new: true})
+    .then((user) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(user);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+module.exports = router;
+
+
+/*
+   {
+        "label": "Hot",
+        "featured": true,
+        "name": "Lacoste",
+        "image": "images/uthappizza.png",
+        "category": "mains",
+        "price": 499,
+        "description": "A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+        "comments": [
+            {
+                "rating": "2",
+                "author": "loh",
+                "comment": "loh pisochniy!!1"
+            }
+        ]
+    }
+
+    {"username": "ufc", "password": "ufc", "firstname": "Vitaly", "lastname": "Iopov"
+*/

44f263c64f6bc3ae74c9db402c68791fb9b2d59a
Dmitry Olkhovikov <59053659+Dmitry-O@users.noreply.github.com>
2021-02-20 20:49:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb+srv://dimasic9420:423282@mongodb.crpbb.mongodb.net/friendsWatcher?retryWrites=true&w=majority',
-    'facebook': {
-        clientId: '1114788815630073',
-        clientSecret: '99883840ed95a9cecfb19d39017517b4'
-    }
+    'mongoUrl': 'mongodb+srv://myusername:mypassword@mongodb.crpbb.mongodb.net/friendsWatcher?retryWrites=true&w=majority'
 }
 
 // 'mongodb://localhost:27017/conFusion',
